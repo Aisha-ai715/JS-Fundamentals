4620a93433b6362827785b9c39e4ad8568cc9dfd
Aisha <aishaelabass@gmail.com>
2025-05-25 17:41:09 +0300

    change for loop to while

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,7 +4,9 @@ let x = Number(arg1);
 if(!x || x<=0){
     console.log('Missing number of occurrences');
 }else{
-    for(let i=0; i<x; i++){
+    let i = 0;
+    while(i<x){
         console.log('C is fun');
+        i++;
     }
 }
