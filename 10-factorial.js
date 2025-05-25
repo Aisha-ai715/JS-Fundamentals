9cf7e4b825cdf93adbb82d39a35ccf1ec162f839
Aisha <aishaelabass@gmail.com>
2025-05-25 20:18:41 +0300

    fix new bug

--- 10-factorial.js
+++ 10-factorial.js
@@ -4,12 +4,11 @@ function factorial(x) {
     if (isNaN(x) || x < 0) {
         return 1; // or throw new Error('Invalid input');
     }
-    if (x === 0) {
+    if (x === 0 || x===1) {
         return 1;
     }
 
-    
-    return x * factorial(n-1);
+    return x * factorial(x-1);
 }
 
 console.log(factorial(arg1));
