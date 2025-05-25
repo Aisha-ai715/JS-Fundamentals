e18e32a8933845dc8b14b2bd7aa7ce505ac26909
Aisha <aishaelabass@gmail.com>
2025-05-25 14:38:01 +0300

    add file 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,7 +3,7 @@ let arg2 =  process.argv[3];
 
 if(!arg1 && !arg2){
     console.log('No argument');
-}else if(arg1 != null || arg2 != null){
+}else if(!arg1 || !arg2){
     console.log('Argument found');
 }else{
     console.log('Argument found');
