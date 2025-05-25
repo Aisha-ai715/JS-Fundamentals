d8efcf486ee0fd69cae00cc95933110d1f753350
Aisha <aishaelabass@gmail.com>
2025-05-25 17:06:19 +0300

    fix another bug

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 let x = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-let i = 0
-while(i<x.length){
-    console.log(x[i]);
-    i++;
+let output = '';
+for(let i =0; i<x.length; i++){
+    output += x[i]+ '\n';
 }
+console.log(output);
